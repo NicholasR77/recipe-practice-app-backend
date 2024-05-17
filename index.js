0f9e27d8d0ff074ccd29b55d62b06c8d4305e32c
Nicholas Robbins <nl7robbins@icloud.com>
2024-05-17 15:07:49 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.post('/recipes', (req, res) => {
+  console.log("🚀 ~ app.get ~ req:", req.body)
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
