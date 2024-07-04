ef63b9ba0de9f214727cff0cbfcc858c14661ebe
Nicholas Robbins <nl7robbins@icloud.com>
2024-07-04 14:28:42 -0700

    Trying to fix connection problems

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
+const sequelize = new Sequelize(process.env.POSTGRES_DB, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
+  host: process.env.POSTGRES_HOST || 'postgres-service-new',
+  port: process.env.POSTGRES_PORT || 5432,
   dialect: 'postgres',
 });
 
