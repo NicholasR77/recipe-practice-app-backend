35751be164ef02ec273a223e9237a7c8446dc4c9
Nicholas Robbins <nl7robbins@icloud.com>
2024-07-05 12:36:40 -0700

    Moving postgres out of helm/k8s

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize(process.env.POSTGRES_DB, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
-  host: process.env.POSTGRES_HOST || 'postgres-service-new',
+  host: process.env.POSTGRES_HOST,
   port: process.env.POSTGRES_PORT || 5432,
   dialect: 'postgres',
 });
