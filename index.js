ef868305c7faa97f81e6fc90a41d78f7404d1a6e
Nicholas Robbins <nl7robbins@icloud.com>
2024-07-16 21:11:22 -0700

    Adding console log to the liveness probe route

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(cors());
 
 app.use('/', (_, res) => res.send('Welcome to the Recipe Practice App'));
 app.get('/healthz', (req, res) => {
+  console.log('Health check');
   res.status(200).send('OK');
 });
 app.use('/recipes', recipeRoutes);
